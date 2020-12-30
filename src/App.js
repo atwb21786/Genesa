bfc74482a8a3043aa7f1747912cef4a36d3f73ad
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-12-30 15:04:40 -0700

    genesa images added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import GenesaDefined from './images/genesadefinition.png';
+import GenesaCover from './images/genesacover.jpg';
+import CubeSphere from './images/CubeSphereBasic-WEB.jpg';
+import CircleGardening from './images/circlegardening.jpg';
+import ThirteenDimensions from './images/13dimensional.jpg';
+import GenesaSixColor from './images/genesa 6 color.jpg';
+import Genesa26Color from './images/genesa 26 color.jpg';
+import GenesaBigForm from './images/genesa big form.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      {/* content goes here */}
+      <img src={GenesaDefined} alt="GenesaDefined"></img><br></br>
+      <img src={CubeSphere} alt="CubeSphere"></img>
+      <img src={CircleGardening} alt="CircleGardening"></img>
+      <img src={ThirteenDimensions} alt="ThirteenDimensions"></img>
+      <img src={GenesaCover} alt="GenesaCover"></img>
+      <img src={GenesaSixColor} alt="GenesaSixColor"></img>
+      <img src={Genesa26Color} alt="Genesa26Color"></img>
+      <img src={GenesaBigForm} alt="GenesaBigForm"></img>
+    </main>
   );
 }
 
